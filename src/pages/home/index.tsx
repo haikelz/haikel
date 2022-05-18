b76458b819256da9f5292c31308d9e53d5887efc
haikelz <haikel.2017th@gmail.com>
2022-05-18 19:39:20 +0700

    Update: styling dikit

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -3,7 +3,7 @@ const Home = () => {
     <section className="body-font tracking-wide">
       <div className="container mx-auto flex py-24 max-w-7xl px-4 sm:px-6 items-center justify-center flex-col h-screen">
         <h1 className="title-font text-3xl mb-1 font-bold">
-          Hello, I'm Haikel 👋
+          Hello, I&#39;m Haikel 👋
         </h1>
         <p className="mb-4 leading-relaxed">Penikmat kopi dan pempek</p>
       </div>
