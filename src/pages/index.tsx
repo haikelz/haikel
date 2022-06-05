a137ed3f49da0d093fd31e1d25f2386c200ec490
haikelz <haikel.2017th@gmail.com>
2022-06-05 21:21:40 +0700

    fix: duh lupa bagian home nya

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,7 +7,7 @@ const Index = () => {
         <title>Home</title>
       </Head>
       <section className="body-font tracking-wide">
-        <div className="container mx-auto flex py-24 max-w-7xl px-4 sm:px-6 items-center justify-center flex-col h-screen">
+        <div className="container mx-auto flex pt-10 md:py-24 max-w-7xl px-4 sm:px-6 items-center justify-center flex-col h-screen">
           <h1 className="title-font text-3xl mb-1 font-bold">
             Hello, I&#39;m Haikel 👋
           </h1>
