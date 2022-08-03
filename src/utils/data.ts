986e57092f784e3ed79b325277be2cf272fd86b0
haikelz <haikel.2017th@gmail.com>
2022-08-03 17:24:41 +0700

    style: adjust color

--- src/utils/data.ts
+++ src/utils/data.ts
@@ -8,7 +8,7 @@ export const ProjectsList = [
     { h2: 'Jadwal Sholat', p: "Website jadwal Sholat dan baca Al-Qur'an", repo: "https://github.com/haikelz/jadwal-sholat", preview: "https://jadwal-sholat.pages.dev/", img: "", stack1: next, stack2: tailwind },
     { h2: "The Sugiharto", p: 'Slicing UI Design bang Nauval', repo: "https://github.com/haikelz/the-sugiharto", preview: "https://the-sugiharto.vercel.app", img: "/img/Project_7.webp", stack1: next, stack2: `framermotion` },
     { h2: "Reddit Posts", p: "A website to get posts from Reddit(mostly memes)", repo: "https://github.com/haikelz/reddit-posts", preview: "ht", img: "", stack1: next, stack2: tailwind },
-    { h2: 'Todo App', p: 'Simple Todo List', repo: 'https://github.com/haikelz/beritakan', preview: "https://beritakan.vercel.app", img: '/img/Project_5.webp', stack1: "ReactJS", stack2: tailwind },
+    { h2: 'Todo App', p: 'Simple Todo List', repo: 'https://github.com/haikelz/todo-app', preview: "https://beritakan.vercel.app", img: '/img/Project_5.webp', stack1: "ReactJS", stack2: tailwind },
     { h2: "Kumpulan Do'a", p: "Kumpulan do'a sehari-hari", repo: "https://github.com/haikelz/kumpulan-doa", preview: "https://kumpulan-doa.pages.dev/", img: "", stack1: next, stack2: chakra },
     { h2: 'Anime Words', p: 'Kumpulan kalimat(bukan motivasi) dari tokoh-tokoh Anime.', repo: 'https://github.com/haikelz/anime-words', preview: 'https://anime-words.vercel.app', img: '/img/Project_2.webp', stack1: next, stack2: chakra },
     { h2: 'Dotfiles', p: 'My Linux configuration for daily use', repo: 'https://github.com/haikelz/dotfiles', preview: '', img: '/img/Project_2.webp', stack1: linux, stack2: bspwm },
