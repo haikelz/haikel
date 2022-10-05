ce00542171d463f2c6191370e3e072f19de7bbd3
haikelz <yuukisakura@proton.me>
2022-10-05 20:38:24 +0700

    fix: retry deployment
    Signed-off-by: haikelz <yuukisakura@proton.me>

--- src/utils/animation.ts
+++ src/utils/animation.ts
@@ -1,13 +1,13 @@
 export const appAnimation = {
-    transition: { duration: 0.4 },
-    initial: { opacity: 0, y: 50 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: 50 },
-}
+  transition: { duration: 0.4 },
+  initial: { opacity: 0, y: 50 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 50 },
+};
 
 export const backToTopAnimation = {
-    transition: { duration: 0.3 },
-    initial: { opacity: 0, y: 50 },
-    animate: { opacity: 1, y: 0 },
-    exit: { opacity: 0, y: 50 },
-}
+  transition: { duration: 0.3 },
+  initial: { opacity: 0, y: 50 },
+  animate: { opacity: 1, y: 0 },
+  exit: { opacity: 0, y: 50 },
+};
