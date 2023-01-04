408ff7a346bf4ec9d89590c071e530a22691b9b4
haikelz <yuukisakura@proton.me>
2023-01-04 21:35:24 +0700

    style(hover): add hover effect in footer
    Signed-off-by: haikelz <yuukisakura@proton.me>

--- src/components/organisms/Footer.tsx
+++ src/components/organisms/Footer.tsx
@@ -10,7 +10,7 @@ const Footer = () => {
           This Website is under{" "}
           <Link
             href="https://github.com/haikelz/website/blob/master/LICENSE"
-            className="hover:text-texthover font-bold"
+            className="font-bold hover:text-crayola"
             passHref
           >
             MIT License
