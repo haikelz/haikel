42d9f1c563c8794fd4c9b2e424cb4ed3fd35647d
haikelz <haikel.2017th@gmail.com>
2023-04-08 06:47:07 +0700

    fix: downgrade next to 13.2.4

--- src/pages/api/og.tsx
+++ src/pages/api/og.tsx
@@ -1,5 +1,6 @@
 /* eslint-disable @next/next/no-img-element */
-import { ImageResponse, NextRequest } from "next/server";
+import { ImageResponse } from "@vercel/og";
+import { NextRequest } from "next/server";
 
 export const config = {
   runtime: "edge",
