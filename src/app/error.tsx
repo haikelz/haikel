07198620061ed2d37c917684c198de7faa5e24e3
Haikel Ilham Hakim <haikel.2017th@gmail.com>
2024-01-03 21:16:01 +0700

    fix: not-found and error page layout

--- src/app/error.tsx
+++ src/app/error.tsx
@@ -7,11 +7,12 @@ import { Heading, Paragraph, UnderlineLink } from "~ui/typography";
 export default function ErrorPage() {
   return (
     <Main className="flex min-h-screen flex-col items-center justify-center text-center">
-      <section className="flex flex-col items-center">
+      <section>
         <TransitionLayout
           transition={{ duration: 0.3 }}
           initial={{ opacity: 0 }}
           animate={{ opacity: 1 }}
+          className="flex flex-col items-center justify-center"
         >
           <Heading as="h1">500 🤖</Heading>
           <Paragraph className="mt-2 font-semibold">
